5ff31d66f265c6599a3257df0e2479ab7e650f65
Lobinux <lobinux@digilearnnials.com>
2022-08-25 23:25:34 -0300

    Player movement WIP

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float jumpForce = 10;
+
+    Rigidbody rb;
+    Vector3 viewDir = Vector3.forward;
+
     public static Action<int> OnJump;
-    private void Update()
+
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    public void JumpToDirection(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+
+        if (input.magnitude > 0)
         {
-            transform.position += new Vector3(0, 0, 1);
-            OnJump((int)transform.position.z);
+            viewDir = new Vector3(input.x, 0, input.y);
+            Vector3 jumpDir = viewDir + transform.up;
+
+            transform.rotation = Quaternion.LookRotation(viewDir, transform.up);
+
+            rb.AddForce(jumpDir * jumpForce);
+            OnJump((int)rb.transform.position.z);
         }
     }
 }
