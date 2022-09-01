78cc4a031a656332a00d9a0337c790838ebf427a
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2022-09-01 09:04:50 -0300

    Fix swap bug

--- Assets/Scripts/PowerUps/SwapPlayersBehaviour.cs
+++ Assets/Scripts/PowerUps/SwapPlayersBehaviour.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SwapPlayersBehaviour : PowerUp
 {
     void Start()
     {
+        user.GetComponent<PlayerMovement>().StopAllCoroutines();
+        affected.GetComponent<PlayerMovement>().StopAllCoroutines();
         Vector3 positionAux = user.transform.position;
         user.transform.position = affected.transform.position;
         affected.transform.position = positionAux;
